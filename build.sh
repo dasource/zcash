c3fa540bc42e327a54ddebc06fd82ec086b10c02
dasource <dasource@es6.com>
2016-10-28 00:16:11 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,11 +8,9 @@ ps axjf
 
 AZUREUSER=$2
 HOMEDIR="/home/$AZUREUSER"
-SHADOWPATH="$HOMEDIR/.shadowcoin"
 VMNAME=`hostname`
 echo "User: $AZUREUSER"
 echo "User home dir: $HOMEDIR"
-echo "User Shadow path: $SHADOWPATH"
 echo "vmname: $VMNAME"
 
 sudo su $2
